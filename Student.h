63348414546cf8aa3905553d7f6209eeb738b010
yuchen1016 <101238787+yuchen1016@users.noreply.github.com>
2022-06-07 16:11:52 +0800

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,28 @@
+#ifndef STUDENT_H 
+#define STUDENT_H
+#include"Course.h"
+#include<string>
+using namespace std;
+
+class Student
+{
+private:
+	string name;
+	string ID;
+	Course course;
+	static int numOfStu;
+public:
+	Student(string, string, Course);
+	Student();
+	void setName(string);
+	string getName();
+	void setID(string);
+	string getID();
+	void setCourse(Course);
+	Course getCourse();
+	void printStudent();
+	static int getNumOfStu();
+	void getPass();
+	void getRanking();
+};
+#endif
